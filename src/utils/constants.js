9addd7b33549a7982a7bfd90833665ee7c1e4338
i-sanyam <sanyam.nitj@gmail.com>
2022-08-28 15:51:35 +0530

    change confusing name withdraw to relinquish

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,16 +4,16 @@ const requestStatusesEnum = {
     'OPEN': 1,
     'APPROVED': 2,
     'REJECTED': 3,
-    'WITHDRAWN': 4,
+    'RELINQUISH': 4,
     'DELETED': 5,
 };
 
 const requestStatusesEnumReverse = {
     1: { name: 'Waiting for Approval', color: 'info'},
     2: { name: 'Access Granted', color: 'success'},
-    3: { name: 'Request Denied', color: 'danger'},
+    3: { name: 'Access Denied', color: 'danger'},
     4: { name: 'Access Relinquished', color: 'secondary'},
-    5: { name: 'Access Revoked', color: 'warning' },
+    5: { name: 'Request deleted', color: 'warning' },
 };
 
 const resourceTypesEnum = {
