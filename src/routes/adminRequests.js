133bb86ba3aa3258281a5000f6a8df22a6428fc1
i-sanyam <sanyam.nitj@gmail.com>
2022-08-31 22:25:56 +0530

    remove commented code for old approval api

--- src/routes/adminRequests.js
+++ src/routes/adminRequests.js
@@ -100,34 +100,6 @@ adminRequestsRouter.post('/approve', ExpressRouteHandler(async (req) => {
 
 module.exports = adminRequestsRouter;
 
-// router.post('/approve', async (req, res) => {
-//     const requestId = req.body.requestId;
-//     console.log(requestId);
-//     // const resourceId = req.body.resourceId;
-//     // const resourceData = await executeQuery('SELECT 1 FROM resources WHERE resource_id = ?', [ resourceId ]);
-//     // if (_.isEmpty(resourceData)) {
-//     //     return res.status(404).send('Resource Id Not Present');
-//     // }
-//     const requestData = await executeQuery(`
-//     SELECT r.name as resourceName FROM resource_requests rr 
-//         JOIN resources r 
-//             ON r.resource_id = rr.resource_id
-//         WHERE rr.request_id = ? AND rr.status = ?`, [ requestId, constants.requestStatus.OPEN ]);
-
-//     if (_.isEmpty(requestData)) {
-//         return res.send("REQUEST NOT FOUND");
-//     }
-
-//     const resourceTypeName = 'github';
-
-//     const fileToRequire = require(`../resource_types/${resourceTypeName}`);
-//     // fileToRequire.prerequisite();
-//     fileToRequire.actions.addAccess(requestData[0].resourceName, 'sanyam-aggarwal-shipsy');
-
-//     await executeQuery('UPDATE resource_requests SET status = ? WHERE request_id = ?', [constants.requestStatus.APPROVED, requestId]);
-//     res.send('ADD REQUESTS');
-// });
-
 // router.post('/withdraw', async (req, res) => {
 //     const requestId = req.body.requestId;
 //     const requestData = await executeQuery(`
