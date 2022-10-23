942b8cd04d0f2150449a156458d2d947f6384fa1
i-sanyam <sanyam.nitj@gmail.com>
2022-10-23 12:08:54 +0530

    cleanup user service

--- src/service/user/index.js
+++ src/service/user/index.js
@@ -1,50 +1,6 @@
 'use strict';
 
-const jwtService = require('../jwt');
 const Mongo = require('../../mongo');
-
-const GITHUB_OAUTH_TYPE = "GITHUB";
-
-const validateGithubUserAndGetCookie = async (userData) => {
-    let userId = null;
-
-    const existingUserData = await Mongo.Users.find({
-        id: userData.id,
-        login: userData.login,
-        OAUTH_TYPE: GITHUB_OAUTH_TYPE,
-    });
-
-    if (!existingUserData || existingUserData.length === 0) {
-        const { insertedId: newUserId } = await Mongo.Users.insertOne({
-            id: userData.id,
-            login: userData.login,
-            avatar_url: userData.avatar_url,
-            OAUTH_TYPE: GITHUB_OAUTH_TYPE,
-            oauth_provider_metadata: userData,
-        });
-        userId = newUserId;
-    } else {
-        userId = existingUserData[0]._id;
-        await Mongo.Users.updateOne({
-            _id: userId,
-        }, {
-            $set: {
-                avatar_url: userData.avatar_url,
-            }
-        });
-    }
-
-    return jwtService.generateJWT({
-        id: userId,
-    });
-};
-
-const validateUserAndGetCookie = async (oauthType, userData) => {
-    if (oauthType === 'GITHUB') {
-        return await validateGithubUserAndGetCookie(userData);
-    }
-    throw new Error('Oauth Method not available');
-};
 
 const getUser = async (userId) => {
     if (!userId) {
@@ -56,6 +12,5 @@ const getUser = async (userId) => {
 }
 
 module.exports = {
-    validateUserAndGetCookie,
     getUser,
 };
